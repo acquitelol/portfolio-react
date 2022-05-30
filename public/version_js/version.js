256785c6643a92631d42150f973e6a8eec267074
Acquite <acquitefx@gmail.com>
2022-05-30 21:38:55 +0100

    cleaned src code and removed all useless parts

--- public/version_js/version.js
+++
@@ -1,9 +0,0 @@
-async function getJson(url) {
-    const response = await fetch(url);
-  
-    return response.json();
-}
-let packageJson = await getJson('./../package.json')
-let desc = document.querySelector('meta[name="description"]');
-let text = "A portfolio of my skills, but this time natively running React with the Ionic CLI! Version "
-desc.setAttribute("content", text+packageJson.version)
